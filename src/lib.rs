c140156e269db7f21e4ade766828409076290258
threadexception <hannes.gaumann@outlook.de>
2023-05-16 12:41:41 +0200

    Even more debugging... yay

--- src/lib.rs
+++ src/lib.rs
@@ -569,7 +569,7 @@ impl<T: Send, M: Metadata, const AUTO_FREE_IDS: bool> ThreadLocal<T, M, AUTO_FRE
 
         // If the bucket doesn't already exist, we need to allocate it
         let bucket_ptr = if bucket_ptr.is_null() {
-            let new_bucket = allocate_bucket::<false, AUTO_FREE_IDS, T, M>(thread.bucket_size(), global_tid_manager(), 0);
+            let new_bucket = allocate_bucket::<false, AUTO_FREE_IDS, T, M>(thread.bucket_size(), global_tid_manager(), thread.bucket);
 
             match bucket_atomic_ptr.compare_exchange(
                 null_mut(),

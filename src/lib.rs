5eefb5df9335df1a74a9d54cec2db5b2f7e43cf9
threadexception <hannes.gaumann@outlook.de>
2023-06-02 01:43:40 +0200

    More debugging

--- src/lib.rs
+++ src/lib.rs
@@ -1065,14 +1065,14 @@ fn allocate_bucket<const ALTERNATIVE: bool, const AUTO_FREE_IDS: bool, T, M: Met
                     println!("calced id: {}[{}]: {}", bucket, n, if bucket == 0 {
                         0
                     } else {
-                        (1 << (bucket - 1)) - 1 + n
+                        (1 << (bucket - 1)) + n
                     });
                     // we need to offset all entries by the number of all entries of previous buckets
                     // (1 << (bucket + 1)) - 1 + n
                     if bucket == 0 {
                         0
                     } else {
-                        (1 << (bucket - 1)) - 1 + n
+                        (1 << (bucket - 1)) + n
                     }
                 },
                 guard: AtomicUsize::new(GUARD_UNINIT),
